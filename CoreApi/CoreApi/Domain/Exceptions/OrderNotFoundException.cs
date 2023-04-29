4cd9be4f94e1e5a5eb2976c7b766c6ccc7bba843
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2023-04-29 20:20:50 +0200

    Refactor

---
+++ CoreApi/CoreApi/Domain/Exceptions/OrderNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace CoreApi.Domain.Exceptions
+
+{
+    public sealed class OrderNotFoundException : Exception
+    {
+        public OrderNotFoundException(int id)
+            : base($"The order with identifier {id} was not found")
+        { }
+
+    }
+}
--- CoreApi/CoreApi/Infrastructure/Persistence/OrderContext.cs
+++ CoreApi/CoreApi/Infrastructure/Persistence/OrderContext.cs
@@ -10,10 +10,13 @@ public class OrderContext : DbContext
 
     #region Configuration
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseCosmos(
-            "https://localhost:8081",
-            "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
-            databaseName: "OrdersDB");
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        string accountEndpoint = configuration["CosmosDbSettings:EndpointUri"];
+        string accountKey = configuration["CosmosDbSettings:PrimaryKey"];
+
+        optionsBuilder.UseCosmos(accountEndpoint, accountKey, databaseName: "OrdersDB");
+    }
     #endregion
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
