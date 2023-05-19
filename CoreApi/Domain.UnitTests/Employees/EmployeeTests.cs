3839679660498d5486bffe05b92a12f8600b9838
SECTORIALES\malcubierre <martin.alcubierre@inetum.com>
2023-05-19 11:20:48 +0200

    Fix test

--- CoreApi/Domain.UnitTests/Employees/EmployeeTests.cs
+++ CoreApi/Domain.UnitTests/Employees/EmployeeTests.cs
@@ -1,24 +1,27 @@
-﻿namespace Domain.UnitTests.Employees;
+﻿using Domain.Entities.Employees;
+
+namespace Domain.UnitTests.Employees;
 
 public class EmployeeTests
 {
 
     [Theory]
     [ClassData(typeof(EmployeeCreateTestData))]
-    public void Create_Should_NotBeEmppty(Guid id, int? trackingNumber, StreetAddress shippingAddress)
+    public void Create_Should_NotBeEmppty(Guid id, string tenantId, string? name, string? surname, string? email, string? dni)
     {
-        var Employee = new Employee(id, trackingNumber, shippingAddress);
+        var employee = new Employee(id, tenantId, name, surname, email, dni);
 
-        Assert.NotEmpty(Employee.ShippingAddress.City);
+        Assert.NotEmpty(employee.Name);
 
     }
 }
 
-public class EmployeeCreateTestData : TheoryData<Guid, int, StreetAddress>
+public class EmployeeCreateTestData : TheoryData<Guid, string, string, string, string, string>
 {
+
     public EmployeeCreateTestData()
     {
 
-        Add(new Guid(), 1, new StreetAddress { Street = "sss", City = "ssss" });
+        Add(new Guid(), "1", "name", "surname", "email", "dni");
     }
 }
